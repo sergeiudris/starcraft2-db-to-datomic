987b0768a491196871165c2948b0ea1a5024c387
sergeiudris <sergei.udris@gmail.com>
2019-01-28 15:29:32 +0300

    app query run using lein run

--- app-query/project.clj
+++ app-query/project.clj
@@ -1,11 +1,20 @@
 (defproject dq.app-query "0.1.0-SNAPSHOT"
+  :main dq.dq
   :plugins [[cider/cider-nrepl "0.18.0"]
             [nightlight/lein-nightlight "RELEASE"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [cider/piggieback "0.3.10"]
                  [figwheel-sidecar "0.5.16"]
-                 [com.datomic/datomic-free "0.9.5544"]
-                 [commons-codec "1.7"]]
-  :repl-options {:init-ns lein.core
+                 [com.datomic/datomic-free "0.9.5656"]
+                 [commons-codec "1.7"]
+                 [io.pedestal/pedestal.service       "0.5.5"]
+                 [io.pedestal/pedestal.service-tools "0.5.5"] ;; Only needed for ns-watching; WAR tooling
+                 [io.pedestal/pedestal.jetty         "0.5.5"]
+                ;  [io.pedestal/pedestal.immutant      "0.5.5"]
+                ;  [io.pedestal/pedestal.tomcat        "0.5.5"]
+                ;  [io.pedestal/pedestal.aws           "0.5.5"]
+                 ]
+  :repl-options {:init-ns dq.dq
+                 :main dq.dq
                 ;  :nrepl-middleware [cider.piggieback/wrap-cljs-repl]
                  })
---
+++ app-query/src/dq/dq.clj
@@ -0,0 +1,85 @@
+(ns dq.dq
+  (:require [datomic.api :as d]
+            [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]))
+
+(def  db-uri "datomic:free://datomicdbfree:4334/hello")
+
+(d/create-database db-uri)
+(def conn (d/connect db-uri))
+(def db (d/db conn))
+
+(defn respond-hello [request]
+  {:status 200 :body "Hello, world!"})
+
+(def routes
+  (route/expand-routes
+   #{["/health" :get respond-hello :route-name :health]}))
+
+(defn create-server []
+  (http/create-server
+   {::http/routes routes
+    ::http/type   :jetty
+    ::http/host "0.0.0.0"
+    ::http/port   8890}))
+
+(defn start []
+  (http/start (create-server)))
+
+(defn -main []
+  (prn conn)
+  (start))
+
+
+
+(comment 
+  
+  (def  uri "datomic:free://localhost:4334/git")
+
+  (d/create-database uri)
+
+  (def conn (d/connect uri))
+
+  conn
+
+  (def db (d/db conn))
+
+  (d/q '[:find ?e :in $] db)
+
+; first query!
+  (map first
+       (d/q '[:find ?repo
+              :where [?e :repo/uri ?repo]]
+            db))
+
+  (map first
+       (d/q '[:find ?ns
+              :where
+              [?e :clj/ns ?n]
+              [?n :code/name ?ns]]
+            db))
+
+  (reduce (fn [agg [o d]]
+            (update-in agg [o] (fnil conj []) d))
+          {}
+          (d/q '[:find ?op ?def
+                 :where
+                 [?e :clj/def ?d]
+                 [?e :clj/defop ?op]
+                 [?d :code/name ?def]]
+               db))
+
+  (defn foo
+    "I don't do a whole lot."
+    [x]
+    (println x "Hello, World!"))
+
+
+  (+ 3 4)
+
+; (hello)
+  
+; (h1)
+  
+  
+  )
