aced7ada37b05a2eaa6f010eb1735730a58e8af8
sergeiudris <sergeiudris@gmail.com>
2019-01-30 08:00:11 +0300

    offset

--- app-query/src/dq/psql.clj
+++ app-query/src/dq/psql.clj
@@ -1,6 +1,7 @@
 (ns dq.psql
   (:require [clojure.repl :refer :all]
             [clojure.java.jdbc :as jdbc]
+            [clojure.pprint :as pp]
             [dq.nrepl]))
 
 (defn hello [] (prn "hello"))
@@ -21,9 +22,18 @@
   (jdbc/query db-spec ["select 3*5 as result"])
 
   (jdbc/query db-spec ["select * from player where tag = 'Scarlett'"])
+
+  (jdbc/query db-spec ["select * from player order by id limit 5 offset 5"])
   
+  (->>
+   (jdbc/query db-spec ["select * from player order by id limit 5 offset 5"])
+   pp/pprint
+   )
   
-  
+   (->>
+    (jdbc/query db-spec ["select id from match order by id limit 5 offset 5"])
+    pp/pprint
+    )
   
   
   )
